8e65cd2bd4233ab64dfa4712f699208ea97b926a
Kukoverov.Dmitry <moby_dik@mail.ru>
2025-08-27 00:03:57 +0300

    создали функцию загрузки картинки

--- DOGS.py
+++ DOGS.py
@@ -1,16 +1,39 @@
 from tkinter import *
+from tkinter import messagebox as mb
 import requests
 from PIL import ImageTk, Image
 from io import BytesIO
 
+def show_image():
+    image_url = get_dog_image()
+    if image_url:
+        try:
+            response = requests.get(image_url, stream=True)
+            response.raise_for_status()
+            img_data = BytesIO(response.content)
+            img = Image.open(img_data)
+
+            img.thumbnail((300, 300))
+            label.congiture(image=img)
+            label.img = img
+        except Exception as e:
+            mb.showerror("Ошибка", f"Не удалось загрузить изображение: {e}")
+
+
+
+
 
 window = Tk()
 window.title("Картинки с сайта")
-window.geometry("3600x4200")
+window.geometry("360x420")
 
 label = Label()
 label.pack(pady=15)
 
 button = Button(text="Загрузить картинку", command=show_image)
 button.pack(pady=15)
-print("Введите ссылку на картинку")
+
+
+
+
+window.mainloop()
